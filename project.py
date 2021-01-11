fe0db44724c44a5ef6077ab612232850bad587b1
Simon <simonctemple@live.com>
2021-01-11 04:04:57 -0800

    testing data types and string methods

--- project.py
+++ project.py
@@ -1,1 +1,20 @@
-print("test")
+# weight = input('How much do you weigh? ')
+# kg = float(weight) / 2.2
+# kg_string = str(kg)
+# print(kg_string[0:5])
+
+
+# first = 'Simon'
+# last = 'Temple'
+
+# message = first + ' [' + last + '] is a coder'
+
+# # use f for formated string. It's easier to visualise
+# msg = f'{first} [{last}] is a coder'
+
+# print(message)
+# print(msg)
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
